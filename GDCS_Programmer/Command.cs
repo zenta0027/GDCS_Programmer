6ba0d5dad03f722884b516ebc9b0b6d904f2d0e1
Zentroll <zenta0027@kaist.ac.kr>
2018-05-09 07:01:30 +0900

    google drive file access

--- GDCS_Programmer/Command.cs
+++ GDCS_Programmer/Command.cs
@@ -4,6 +4,18 @@ using System.Globalization;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace GDCS_Programmer
 {
     /// <summary>
@@ -49,6 +61,70 @@ namespace GDCS_Programmer
             }
         }
 
+        private static string LoadGoogleDocs()
+        {
+            string[] Scopes = { DriveService.Scope.DriveReadonly };
+            string ApplicationName = "Drive API .NET Quickstart";
+            string returnstring = "";
+
+            UserCredential credential;
+
+            Console.WriteLine("Hello!");
+            returnstring = returnstring + "Hello! ";
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+                returnstring = returnstring + "Credential file saved to: " + credPath;
+            }
+
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 100;
+            listRequest.Fields = "nextPageToken, files(id, name)";
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
+                .Files;
+            Console.WriteLine("Files:");
+            returnstring += "Files:";
+            returnstring += files.Count;
+            if (files != null && files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    Console.WriteLine("{0} ({1})", file.Name, file.Id);
+                    returnstring += string.Format("{0} ({1})", file.Name, file.Id);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No files found.");
+                returnstring += "No files found.";
+            }
+            Console.Read();
+            return returnstring;
+
+        }
+
         /// <summary>
         /// Gets the instance of the command.
         /// </summary>
@@ -87,8 +163,13 @@ namespace GDCS_Programmer
         /// <param name="e">Event args.</param>
         private void MenuItemCallback(object sender, EventArgs e)
         {
+
             string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
             string title = "Command";
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            message = LoadGoogleDocs();
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
             // Show a message box to prove we were here
             VsShellUtilities.ShowMessageBox(
