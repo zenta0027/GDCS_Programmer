be19445857d876e6ed0d7bb4a1ec0ec7587cf733
Zentroll <zenta0027@kaist.ac.kr>
2018-05-09 10:50:34 +0900

    open tag file

--- GDCS_Programmer/Command.cs
+++ GDCS_Programmer/Command.cs
@@ -65,6 +65,8 @@ namespace GDCS_Programmer
 
         private static string LoadGoogleDocs()
         {
+            string tagId;
+            string folderId;
             IList<Google.Apis.Drive.v3.Data.File> Files = new List<Google.Apis.Drive.v3.Data.File>();
             string[] Scopes = { DriveService.Scope.DriveReadonly };
             string ApplicationName = "Drive API .NET Quickstart";
@@ -118,7 +120,85 @@ namespace GDCS_Programmer
                     if (file.Name == g_TagFileName)
                     {
                         returnstring += string.Format("{0} ({1})\n", file.Name, file.Id);
+                        tagId = file.Id;
+                        if (file.Parents.Count > 0)
+                        {
+                            folderId = file.Parents[0];
+                        }
+                    }
+                    Console.WriteLine("{0} ({1})", file.Name, file.Id);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No files found.");
+                returnstring += "No files found.";
+            }
+            Console.Read();
+            return returnstring;
 
+        }
+
+        private static string LoadTagFile()
+        {
+            string tagId;
+            string folderId;
+            IList<Google.Apis.Drive.v3.Data.File> Files = new List<Google.Apis.Drive.v3.Data.File>();
+            string[] Scopes = { DriveService.Scope.DriveReadonly };
+            string ApplicationName = "Drive API .NET Quickstart";
+            string returnstring = "";
+
+            UserCredential credential;
+
+            Console.WriteLine("Hello!");
+            returnstring = returnstring + "Hello! ";
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+                returnstring = returnstring + "Credential file saved to: " + credPath;
+            }
+
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 1000;
+            listRequest.Fields = "nextPageToken, files(id, name)";
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
+                .Files;
+            Console.WriteLine("Files:");
+            returnstring += "Files:";
+            returnstring += files.Count;
+
+            if (files != null && files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    if (file.Name == g_TagFileName)
+                    {
+                        returnstring += string.Format("{0} ({1})\n", file.Name, file.Id);
+                        tagId = file.Id;
+                        folderId = file.Parents[0];
+                        break;
                     }
                     Console.WriteLine("{0} ({1})", file.Name, file.Id);
                 }
